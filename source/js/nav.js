80cdb9e78984f402099b62d40edbc6ea80f2b124
Kostikov Mihail <kostikovmu@ya.ru>
2018-08-27 15:32:40 +0300

    mobile navigation was added

--- source/js/nav.js
+++ source/js/nav.js
@@ -5,17 +5,33 @@ $(function(){
   $("body").on('click', '[href*="#"]', function(e){
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top}, 1000);
     e.preventDefault();
+    if($(document).width() < 661) {
+      menuOff();
+    }
   });
 
+  $('body').on('click', '.nav__button', menuOn);
+
 });
+
+function menuOn() {
+  $('.nav__menu').show();
+  $('.fi-nav-icon-a').removeClass('fi-nav-icon-a').addClass('fi-close-a');
+  $('body').off('click', '.nav__button', menuOn).on('click', '.nav__button', menuOff);
+}
+
+function menuOff() {
+  $('.nav__menu').fadeOut();
+  $('.fi-close-a').removeClass('fi-close-a').addClass('fi-nav-icon-a');
+  $('body').off('click', '.nav__button', menuOff).on('click', '.nav__button', menuOn);
+}
 
 function onScroll(event){
   var scrollPosition = $(document).scrollTop();
   $('.nav__link').each(function () {
     var currentLink = $(this);
     var refElement = $(currentLink.attr("href"));
-    // var elementPadding = Number(refElement["0"].css('padding').replace('px', ''));
-    if (refElement.position().top - 90  <= scrollPosition && refElement.position().top + refElement.height() + 90 > scrollPosition) {
+    if (refElement.position().top - 30   <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
       currentLink.addClass("nav__link--active");
     }
     else{
